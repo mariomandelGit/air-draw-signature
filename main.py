3b66b8a1e7656681fbaf9eb1bfe8f4412d599c60
Colton Homuth <chomuth@uccs.edu>
2020-12-12 16:00:16 -0700

    test

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import tkinter as tk
 import tkinter.font as font
-from PIL import Image
+import signature
+
+
+def openNewWindow(window):
+    # Toplevel object which will
+    # be treated as a new window
+    newWindow = tk.Toplevel(window)
+
+    # sets the title of the
+    # Toplevel widget
+    newWindow.title("New Window")
+
+    # sets the geometry of toplevel
+    newWindow.geometry("200x200")
+
+    # A Label widget to show in toplevel
+    tk.Label(newWindow, text="This is a new window").pack()
+
+
+def openSigWindow():
+    signature()
 
 
 class Application():
+
     def __init__(self):
         window = tk.Tk()
         window.title('Air Signature')
         window.state("zoomed")
         window.config(bg= '#D9CB8D')
         buttonFont = font.Font(family='Courier', size=25, weight='bold')
-        tk.Button(text='Draw Your Signature', height=4, width=25, bg='grey', font=buttonFont).place(x=380, y=125)
+        tk.Button(text='Draw Your Signature', height=4, width=25, bg='grey', font=buttonFont, command=openSigWindow).place(x=380, y=125)
         tk.Button(text='Save Your Signature', height=4, width=25, bg='grey', font=buttonFont).place(x=80, y=375)
         tk.Button(text='Free Draw', height=4, width=25, bg='grey', font=buttonFont).place(x=680, y=375)
         window.mainloop()
